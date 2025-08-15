2ccbcd375e9bc7ee77e09b79a14e4ca3bf8a818a
Marzi Ghasedian <marzighased@gmail.com>
2025-08-15 12:57:12 +0200

    push and pop Method

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-const numbers = [1, 3, 4, 5, 7];
+//Array Definition and Length
+
+/*const numbers = [1, 3, 4, 5, 7];
 const colors = new Array("blue", "red", "green");
 
+//console.log(numbers);
+//console.log(colors);
+//console.log(numbers[1]);
+
+//numbers[0] = 100;
 console.log(numbers);
-console.log(colors);
-console.log(numbers[1]);
+//console.log(numbers.length);
+numbers.length = 3;
+console.log(numbers);
+*/
+
+//push ==> add to End of the Array
+
+const fruits = ["apple", "orange", "banana"];
+console.log(fruits);
+const newFruits = fruits.push("grape", "cherry");
+console.log(fruits);
+console.log(newFruits);
+
+//pop ==> delete from End of the Array
+
+fruits.pop();
+console.log(fruits);
+console.log(fruits.pop());
+console.log(fruits.pop());
+console.log(fruits);
+
+
+
