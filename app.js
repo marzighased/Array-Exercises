15ccdb8b9269bb1239fae6124315d68cc561dc9c
Marzi Ghasedian <marzighased@gmail.com>
2025-08-18 20:06:10 +0200

    shift/unshift - slice/splice Method

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ console.log(numbers);
 
 //push ==> add to End of the Array
 
-const fruits = ["apple", "orange", "banana"];
-console.log(fruits);
+//const fruits = ["apple", "orange", "banana"];
+/*console.log(fruits);
 const newFruits = fruits.push("grape", "cherry");
 console.log(fruits);
 console.log(newFruits);
@@ -29,6 +29,33 @@ console.log(fruits);
 console.log(fruits.pop());
 console.log(fruits.pop());
 console.log(fruits);
+*/
 
+//shift ==> delete from the Begining of the Array
 
+//console.log(fruits.shift());
+//console.log(fruits);
 
+//unshift ==> add to the Begining of the Array
+
+//fruits.unshift("kiwi", "peach");
+//console.log(fruits);
+
+//splice - slice
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//array.slice(start,end)
+
+//const slicedNumbers1 = numbers.slice(2, 5);
+//const slicedNumbers2 = numbers.slice(2);
+
+//console.log(slicedNumbers2);
+console.log(numbers);
+
+//array.splice(start, deleteCount, item1, ..., itemN)
+
+//numbers.splice(4, 3);
+numbers.splice(4, 3, 101, 102, 103);
+
+console.log(numbers);
